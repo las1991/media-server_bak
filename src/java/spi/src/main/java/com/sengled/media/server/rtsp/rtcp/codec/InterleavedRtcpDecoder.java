04f085434cf5bbeaabed9e67b202115c3f9a6afc
las <las@sengled.com>
2018-09-28 14:33:20 +0800

    增加rtcp解码

---
+++ src/java/spi/src/main/java/com/sengled/media/server/rtsp/rtcp/codec/InterleavedRtcpDecoder.java
@@ -0,0 +1,29 @@
+package com.sengled.media.server.rtsp.rtcp.codec;
+
+import com.sengled.media.server.rtsp.rtp.FramingRtpPacket;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * @author las
+ * @date 18-9-28
+ */
+public class InterleavedRtcpDecoder extends MessageToMessageDecoder<FramingRtpPacket> {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(InterleavedRtcpDecoder.class);
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        LOGGER.error("{}", cause.getMessage(), cause);
+    }
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, FramingRtpPacket msg, List<Object> out) throws Exception {
+        //TODO decode
+        LOGGER.debug("rtcp {}", msg);
+    }
+}
